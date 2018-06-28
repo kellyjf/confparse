cae4b40f69ab28fcf11cce0ae8b51eef35c32689
John Kelly <jkelly@aircell.com>
2018-06-28 15:43:40 -0600

    Handle spaces in file names

--- cole.sh
+++ cole.sh
@@ -5,7 +5,7 @@ sqlite3 tails.sqlite < cole.sql
 for file in current/*gz; do
 	echo "===============  $file ================="
 	rm -rf var
-	tar xf $file
+	tar xf "$file"
 	./cole.py
 	rm -rf var
 done
