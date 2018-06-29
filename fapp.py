63e49bac8be0a922e8032bce4fa5e8f8b26f2f1d
John Kelly <jkelly@aircell.com>
2018-06-29 09:34:12 -0600

    check

--- fapp.py
+++ fapp.py
@@ -4,9 +4,6 @@
 from flask import Flask,render_template,url_for
 import sqlite3 as sq
 
-db=sq.connect("tails.sqlite")
-db.row_factory=sq.Row
-curr=db.cursor()
 
 app=Flask(__name__)
 
@@ -14,11 +11,19 @@ app=Flask(__name__)
 def tails(id):
 	return "Tail {}".format(id)
 
+@app.route("/system/<id>")
+def system(id):
+	return "ID is {}".format(id)
+
 @app.route("/systems")
 def systems():
+	db=sq.connect("tails.sqlite")
+	db.row_factory=sq.Row
+	curr=db.cursor()
 	ret="<html><table>"
 	systems=curr.execute("select version as schema,count(*) as count from systems group by 1 order by 1")	
 	systems=systems.fetchall()
+	curr.close()
 	return render_template('out.html',systems=systems)
 	
 app.run(host="0.0.0.0", port=5555)
